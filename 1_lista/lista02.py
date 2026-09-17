b748b70f77d7b96c564a18a1618710533869ea1b
Willame Oliveira <willame.oliveira@ifpi.edu.br>
2026-09-17 13:24:57 -0300

    melhoria lista02

--- 1_lista/lista02.py
+++ 1_lista/lista02.py
@@ -1,23 +1,27 @@
 """
-Implementando redimensionamento de array a partir da cópia para um novo array maior
+Fazendo o array de tamanho 3 guardar um número a mais.
+Implementando a partir da cópia para um novo array maior.
 """
 
 capacidade = 3
 lista = [None] * capacidade  # Similar a escrever lista = [None, None, None]
-print(lista)
 
 lista[0] = 1
 lista[1] = 2
 lista[2] = 3
 
+print("LISTA ATUAL")
 for i in range(3):
     print(lista[i])
 
-capacidade = 4
 nova_lista = [None] * 4
 
 for i in range(3):
     nova_lista[i] = lista[i]
 
-del lista
-print(nova_lista)
+nova_lista[3] = 4
+del lista  #  Solicitando que o python apague a lista antiga da memória
+
+print("NOVA LISTA")
+for i in range(4):
+    print(nova_lista[i])
