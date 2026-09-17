e141182a35d79bdecedbd66b7135a43da720b079
Willame Oliveira <willame.oliveira@ifpi.edu.br>
2026-09-17 13:38:23 -0300

    lista04 melhoria

--- 1_lista/lista04.py
+++ 1_lista/lista04.py
@@ -3,7 +3,8 @@ Implemente uma função que recebe uma lista, o tamanho dela e um número inteir
 Se o número existe na lista, a função retorna True. Em caso contrário, retorna False.
 Depois, crie uma lista com 5 números inteiros e teste sua função.
 
-OBS: A resposta abaixo está incompleta. Finalize a função pesquisar. Não use a palavra reservada in.
+OBS: A resposta abaixo está incompleta. Finalize a função pesquisar. 
+Intruções: Não use a palavra reservada in. Use um for para percorrer a lista e comparar cada número dela com o número pesquisado.
 """
 
 def pesquisar(lista, tamanho, numero):
