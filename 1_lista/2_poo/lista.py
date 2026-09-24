357dc2eb22266eabb1dc1e9ff14be38d9652d31e
Willame Oliveira <willame.oliveira@ifpi.edu.br>
2026-09-24 09:32:47 -0300

    Melhoria comentários

--- 1_lista/2_poo/lista.py
+++ 1_lista/2_poo/lista.py
@@ -19,16 +19,19 @@ class Lista:
         return -1
 
     def obter(self, posicao):
+        """ Retorna o número guardado em uma determinada posição da lista.
+        Caso a posição não exista na lista, retorna None. """
         pass
 
     def inserir(self, posicao, numero):
         """ Adiciona um número em uma posição da lista
-         empurrando os demais números para o fim da lista """
+         empurrando os demais números para o fim da lista.
+          Se tem espaço na lista, retorna True, se não, False. """
         pass
     
     def remover(self, posicao):
         """ Remove um número de uma posição da lista
-        e puxa os demais números para deixar o espaço vazio no fim da lista """
+        e puxa os demais números para deixar o espaço vazio no fim da lista.
+        Ao final, retorna True. Caso a posição não exista na lista, retorna False. """
         pass
-
     
